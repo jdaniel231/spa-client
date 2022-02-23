519f554d168e2f60a4fbb44c6d234657030cd8dc
hammer200911 <jdaniel231@gmail.com>
2022-02-23 09:22:02 -0300

    axios implmentaçao show movies

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:3000/api"
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    categories: [],
+    category: {},
   },
   mutations: {
+    setCategories(state, categories) {
+      state.categories = categories
+    },
+
+    setCategory(state, category) {
+      state.category = category
+    },
   },
   actions: {
+    fetchCategories({commit}) {
+      axios.get("http://localhost:3000/api/categories")
+        .then(response => commit('setCategories', response.data))
+        .catch(err => console.log(err.response))
+    },
+
+    getCategory({commit}, id) {
+      axios.get(`http://localhost:3000/api/categories/${id}`)
+      .then(response =>{ commit('setCategory', response.data)})
+      .catch(err => console.log(err.response))
+    }
   },
   modules: {
   }
---
+++ src/views/dashboard/children/categories/Index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  #CategoriesIndex 
+    .columns.is-multiline
+      .column.is-4(v-for="category in categories" :key="category.id")
+        router-link(:to="{ name: 'category_path', params: { id: category.id } }")
+          .notification
+            h3.title {{ category.name }}
+            h4.subtitle.has-text-grey-light {{ category.description }}
+    
+</template>
+
+<script>
+ import { mapState } from 'vuex'
+
+export default {
+  name: 'CategoriesIndex',
+
+  computed: mapState({
+      categories: state => state.categories
+    }),
+  mounted() {
+      this.$store.dispatch('fetchCategories')
+    }
+}
+</script>
